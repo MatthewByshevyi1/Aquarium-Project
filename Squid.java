91a94230f3a0f8b989c697b3a26460b2bbf7e4d8
MatthewByshevyi1 <281byshevyim@student.dist113.org>
2026-08-24 02:26:22 +0000

    Added Squid Subclass

---
+++ Squid.java
@@ -0,0 +1,19 @@
+public class Squid extends SeaCreature{
+    
+    private String symbol;
+    private int moveCounter = 0;
+    
+    public Squid (String name, int position, int speed, int direction, String s) {
+        super(name, position, speed, direction);
+        symbol = s;
+    }
+
+    @Override
+    public void move(int tankWidth) {
+        if (moveCounter == 1) {
+            position += speed * 
+        } 
+    }
+}
+
+ "<☲>≼≼≼"
