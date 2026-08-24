1ebd3dd6e4867058889d2b19101f1e2959a675ee
MatthewByshevyi1 <281byshevyim@student.dist113.org>
2026-08-24 02:29:59 +0000

    Added Squid subclass

--- Squid.java
+++ Squid.java
@@ -2,6 +2,7 @@ public class Squid extends SeaCreature{
     
     private String symbol;
     private int moveCounter = 0;
+    private int inkCounter = 0;
     
     public Squid (String name, int position, int speed, int direction, String s) {
         super(name, position, speed, direction);
@@ -11,9 +12,28 @@ public class Squid extends SeaCreature{
     @Override
     public void move(int tankWidth) {
         if (moveCounter == 1) {
-            position += speed * 
-        } 
+            position += speed * direction;
+            moveCounter = 0;
+        }
+        else {
+            moveCounter++;
+        }
+        loseTentacles();
     }
+
+    @Override
+    public String getSymbol() {
+        if (inkCounter != 2)
+            return symbol;
+        return "\u001B[48;5;244m" + symbol + "\u001B[0m";
+    }
+
+    public void loseTentacles() {
+        if ((int)(Math.random() * 10) == 1 && symbol.length() > 4) {
+            symbol = symbol.substring(0,(symbol.length()-1));
+        }
+    }
+
 }
 
- "<☲>≼≼≼"
+// "<☲>≼≼≼"
